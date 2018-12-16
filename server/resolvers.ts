fa6e09327a39c040927f864b50a169f8b5d4559f
Deivid Byrdarov <deivid.byrdarov@gmail.com>
2018-12-17 01:01:33 +0200

    Login, register

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -1,7 +1,50 @@
+import User from "./models/User"
+import { hash, compare } from "bcrypt"
+
+const HASH_SALT = 10
+
 export default {
   Query: {
-    hello: () => {
-      return "Hello World!"
+    register: async (parent, { email, username, password }, ctx) => {
+      let user
+
+      try {
+        user = await User.create({
+          email,
+          username,
+          password: await hash(password, HASH_SALT),
+        })
+      } catch (err) {
+        return {
+          ok: false,
+          error: [err],
+        }
+      }
+
+      return {
+        ok: true,
+        user,
+      }
+    },
+    login: async (parent, { email, password }, ctx) => {
+      const user = await User.findOne({ email })
+
+      if (await compare(password, user.password)) {
+        return {
+          ok: true,
+          user,
+        }
+      }
+
+      return {
+        ok: false,
+        error: [
+          {
+            field: "",
+            message: "Invalid credentials",
+          },
+        ],
+      }
     },
   },
 }
--- server/schema.ts
+++ server/schema.ts
@@ -1,7 +1,35 @@
 import { gql } from "apollo-server-express"
 
 export default gql`
+  type User {
+    id: ID!
+    email: String!
+    username: String!
+  }
+
+  type RegisterResponse {
+    ok: Boolean!
+    user: User
+    error: [Error!]
+  }
+
+  type LoginResponse {
+    ok: Boolean!
+    user: User
+    error: [Error!]
+  }
+
+  type Error {
+    field: String!
+    message: String!
+  }
+
   type Query {
-    hello: String!
+    register(
+      email: String!
+      username: String!
+      password: String!
+    ): RegisterResponse!
+    login(email: String!, password: String!): LoginResponse!
   }
 `
