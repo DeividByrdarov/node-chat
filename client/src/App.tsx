da74884e4f9dc1a3a82b8c6aa2b5bda16313f71f
Deivid Byrdarov <deivid.byrdarov@gmail.com>
2018-12-17 22:04:27 +0200

    Changed client graphql connection to work with subsciptions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import * as React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import ApolloClient from "apollo-boost"
+import { InMemoryCache } from "apollo-boost"
+import { ApolloClient } from "apollo-client"
 import { ApolloProvider } from "react-apollo"
+import { split } from "apollo-link"
+import { HttpLink } from "apollo-link-http"
+import { WebSocketLink } from "apollo-link-ws"
+import { getMainDefinition } from "apollo-utilities"
 
 import ContextProvider from "./Context"
 import Home from "./components/Home"
@@ -9,8 +14,37 @@ import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
 import Logout from "./components/auth/Logout"
 
-const client = new ApolloClient({
+// Create an http link:
+const httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql",
+})
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:5000/`,
+  options: {
+    reconnect: true,
+  },
+})
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  httpLink
+)
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
 })
 
 class App extends React.Component {
