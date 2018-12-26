abe590b9f1f0e23020b0f0eab0d87c9ac9761937
Deivid Byrdarov <deivid.byrdarov@gmail.com>
2018-12-26 21:43:40 +0200

    Use env variable for db url

--- server/config/db.ts
+++ server/config/db.ts
@@ -2,7 +2,7 @@ import * as mongoose from "mongoose"
 
 export default async () => {
   await mongoose.connect(
-    "mongodb://localhost/node-chat",
+    process.env.DATABASE_URL || "mongodb://localhost/node-chat",
     {
       useNewUrlParser: true,
     }
